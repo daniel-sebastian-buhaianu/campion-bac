839164b221bcb73801eb9806bfb500cea95d0d9c
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-10-05 11:31:11 +0300

    Update bac.cpp

--- bac.cpp
+++ bac.cpp
@@ -8,25 +8,31 @@ using namespace std;
 ifstream fin("bac.in");
 ofstream fout("bac.out");
 
+int c[PMAX][CMAX], nrc[PMAX], b[2*CMAX], nrb;
+
 int minute(char ora[]);
 
 int main()
 {
-	int K, L, A, B, i, c[PMAX][CMAX], st[PMAX], dr[PMAX], b[2*CMAX], nrc, nrb, p;
+	int K, L, A, B, i, p, ok, st, dr, mij;
 	char ora[6];
 
 	fin >> K >> L;
 
 	fin >> A;
 
+	nrc[0] = A;
+
 	for (i = 0; i < A; i++)
 	{
 		fin >> ora;
-
+		
 		c[0][i] = minute(ora);
 	}
 
 	fin >> B;
+
+	nrc[1] = B;
 
 	for (i = 0; i < B; i++)
 	{
@@ -37,51 +43,110 @@ int main()
 
 	fin.close();
 
-	st[0] = st[1] = 0;
-	dr[0] = A-1;
-	dr[1] = B-1;
-
-	nrc = nrb = 0;
-
-	while (nrc < A+B)
+	do
 	{
-		if (st[0] <= dr[0] && st[1] <= dr[1])
+		// aleg portul din care incep prima cursa
+		if (nrc[0] && nrc[1])
 		{
-			p = c[0][st[0]] < c[1][st[1]] ? 0 : 1;
+			p = c[0][0] < c[1][0] ? 0: 1;
 		}
-		else if (st[0] <= dr[0])
+		else if (nrc[0])
 		{
 			p = 0;
 		}
-		else if (st[1] <= dr[1])
+		else if (nrc[1])
 		{
 			p = 1;
 		}
 
-		nrc++;
-		b[nrb++] = c[p][st[p]] + K + L;
-		st[p]++;
+		b[nrb] = c[p][0]+K+L, nrc[p]--;
+
+		for (i = 0; i < nrc[p]; i++)
+		{
+			c[p][i] = c[p][i+1];
+		}
+
+		// dupa prima cursa am ajuns in portul opus
 		p = !p;
-
-		while (nrc < A+B
-			&& st[p] <= dr[p]
-			&& b[nrb-1] <= c[p][st[p]])
+		
+		// caut urm. cursa pe care o pot face din portul p
+		// cu aceeasi barca
+		for (ok = 1; ok && nrc[p];)
 		{
-			nrc++;
-			b[nrb-1] = c[p][st[p]] + K + L;
-			st[p]++;
-			p = !p;
+			st = -1, dr = nrc[p];
+
+			while (dr-st > 1)
+			{
+				mij = st + (dr-st)/2;
+				
+				if (c[p][mij] < b[nrb])
+				{
+					st = mij;
+				}
+				else
+				{
+					dr = mij;
+				}
+			}
+
+			if (dr == nrc[p] || c[p][dr] < b[nrb])
+			{
+				ok = 0;
+			}
+			else
+			{
+				b[nrb] = c[p][dr]+K+L, nrc[p]--;
+
+				for (i = dr; i < nrc[p]; i++)
+				{
+					c[p][i] = c[p][i+1];
+				}
+
+				// dupa cursa am ajuns in portul opus
+				p = !p;
+			}
 		}
 
-		while (nrc < A+B 
-			&& st[!p] <= dr[!p]
-			&& b[nrb-1] + K <= c[!p][st[!p]])
+		// nu mai pot face nicio cursa in portul p
+		// caut urm. cursa pe care o pot face din portul opus (!p)
+		// cu aceeasi barca
+		for (ok = 1; ok && nrc[!p];)
 		{
-			nrc++;
-			b[nrb-1] = c[!p][st[!p]] + K + L;
-			st[!p]++;
+			st = -1, dr = nrc[!p];
+		
+			while (dr-st > 1)
+			{
+				mij = st + (dr-st)/2;
+
+				if (c[!p][mij] < b[nrb]+K)
+				{
+					st = mij;
+				}
+				else
+				{
+					dr = mij;
+				}
+			}
+
+			if (dr == nrc[!p] || c[!p][dr] < b[nrb]+K)
+			{
+				ok = 0;
+			}
+			else
+			{
+				b[nrb] = c[!p][dr]+K+L, nrc[!p]--;
+
+				for (i = dr; i < nrc[!p]; i++)
+				{
+					c[!p][i] = c[!p][i+1];
+				}
+			}
 		}
+		
+		// nu mai pot face nicio cursa cu aceasta barca
+		nrb++;
 	}
+	while(nrc[0] || nrc[1]);
 
 	fout << nrb;
 	
@@ -101,4 +166,4 @@ int minute(char ora[6])
 
 	return HH*60 + MM;
 }
-// scor 20
+// scor 40
